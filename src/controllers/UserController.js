f9ea1dcb79b04b11feec5bb7fc1841e3e3e7bb79
daavii-b <britodavi122@gmail.com>
2022-11-27 19:58:43 -0300

    routes: created route for create, update, delete, index and show, create a controller for each one too.

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -8,8 +8,92 @@ class UserController {
 
       return res.json(user);
     } catch (e) {
-      console.log(e);
-      return res.status(400).json('error');
+      return res.status(400).json(
+        {
+          errors: e.errors.map((error) => error.message),
+        },
+      );
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+
+      return res.status(200).json(users);
+    } catch (e) {
+      return res.status(500).json('Internal Server Error');
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { username } = req.params;
+      const user = await User.findOne({
+        where: {
+          username,
+        },
+      });
+
+      if (!user) {
+        return res.status(404).json(
+          {
+            errors: ['User not found'],
+          },
+        );
+      }
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(500).json('Internal Server Error');
+    }
+  }
+
+  async upgrade(req, res) {
+    try {
+      const { username } = req.params;
+
+      const user = await User.findOne({
+        where: {
+          username,
+        },
+      });
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User not found'],
+        });
+      }
+
+      await user.update(req.body);
+
+      return res.status(200).json(user);
+    } catch (e) {
+      return res.status(500).json('Internal Server Error');
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { username } = req.params;
+
+      const user = await User.findOne({
+        where: {
+          username,
+        },
+      });
+
+      if (!user) {
+        return res.status(404).json('User not found');
+      }
+
+      await user.destroy();
+
+      return res.status(204).json({
+        message: 'User deleted successfully',
+      });
+    } catch (e) {
+      return res.status(500).json('Internal Server Error');
     }
   }
 }
